617bfcfda23a810c58dd902ab5fcb1ca832ecc05
bollanarender <97560363+bollanarender@users.noreply.github.com>
2022-10-13 23:17:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Greet from "./components/Greet.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Greet />
+    </div>
+  );
+}
---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+function Greet() {
+  return (
+    <div>
+      <h1>Hi narender</h1>
+    </div>
+  );
+}
+export default Greet;
